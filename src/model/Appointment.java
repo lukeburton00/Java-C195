8d43ba5ae04ee02c695dd399a053ce16f8cf62fd
lukeburton00 <lukesburton19@gmail.com>
2022-11-20 15:55:38 -0700

    initial commit

---
+++ src/model/Appointment.java
@@ -0,0 +1,10 @@
+package model;
+
+public class Appointment {
+    int id;
+    String title;
+    String description;
+    String location;
+    String type;
+
+}
---
+++ src/util/JDBC.java
@@ -0,0 +1,6 @@
+package util;
+
+public abstract class JDBC {
+
+
+}
