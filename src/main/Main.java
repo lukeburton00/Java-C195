ec96f02b522dbc7fce7dcfbb3cf5a8c7587a053b
lukeburton00 <lukesburton19@gmail.com>
2022-11-23 10:26:35 -0700

    establishes connection to MySQL

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,19 +5,25 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import util.JDBC;
+
+import java.util.Objects;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("/view/log_in_form.fxml"));
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/view/log_in_form.fxml")));
         primaryStage.setTitle("C195 PA");
         primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.show();
     }
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
+        JDBC.openConnection();
         launch(args);
+        JDBC.closeConnection();
     }
 }
--- src/util/JDBC.java
+++ src/util/JDBC.java
@@ -1,6 +1,52 @@
 package util;
 
-public abstract class JDBC {
+import javafx.scene.chart.ScatterChart;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public abstract class JDBC
+{
+    private static final String protocol = "jdbc";
+    private static final String vendor = ":mysql:";
+    private static final String location = "//localhost/";
+    private static final String databaseName = "client_schedule";
+    private static final String jdbcUrl = protocol + vendor + location + databaseName + "?connectionTimeZone = SERVER";
+
+    private static final String driver = "com.mysql.cj.jdbc.Driver";
+
+    private static final String userName = "sqlUser";
+    private static final String password = "Passw0rd!";
+
+    public static Connection connection;
+
+    public static void openConnection()
+    {
+        try
+        {
+            Class.forName(driver);
+             connection = DriverManager.getConnection(jdbcUrl,userName,password);
+             System.out.println("Connection to DB successful.");
+
+        }
+
+        catch(Exception e)
+        {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+
+    public static void closeConnection()
+    {
+        try
+        {
+            connection.close();
+            System.out.println("Connection to DB closed.");
+        }
+        catch(Exception e)
+        {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
 
 }
