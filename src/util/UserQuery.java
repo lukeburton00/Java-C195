4eae95ec49ae94d051837d1bbb7815948ed2c2cf
lukeburton00 <lukesburton19@gmail.com>
2023-01-02 18:34:43 -0700

    insert user query created

--- src/util/UserQuery.java
+++ src/util/UserQuery.java
@@ -1,10 +1,23 @@
 package util;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public abstract class UserQuery {
 
-    public static int insert(String userName, String password)
-    {
-        String sql = "INSERT INTO users (User_Name. Password) VALUES(?,?)";
-        return 0;
+    public static int insert(String userName, String password) throws SQLException {
+        // The SQL Statement to execute
+        String sqlStatement = "INSERT INTO users (User_Name, Password) VALUES(?,?)";
+
+        // Prepare the SQL Statement for setting variables
+        PreparedStatement preparedStatement = JDBC.connection.prepareStatement(sqlStatement);
+
+        // Set variables in the SQL Statement
+        preparedStatement.setString(1, userName);
+        preparedStatement.setString(2,password);
+
+        // Execute Statement and return number of rows affected
+        int rowsAffected = preparedStatement.executeUpdate();
+        return rowsAffected;
     }
 }
